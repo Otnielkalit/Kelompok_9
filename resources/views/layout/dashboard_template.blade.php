db8f72df9e8ca21c1f78f6c4710b1a12da65f88b
Otnielkalit <otnielkalit25@gmail.com>
2023-12-09 22:50:57 +0700

    tes UI add poin penilaian in admin page

--- resources/views/layout/dashboard_template.blade.php
+++ resources/views/layout/dashboard_template.blade.php
@@ -77,7 +77,7 @@
 
             <li class="nav-item">
                 <a class="nav-link text-white {{ Route::currentRouteName() === 'poin_penilaian' ? 'active bg-gradient-primary' : '' }}"
-                    href="/poin-penilaian">
+                    href="/poin-penilaian" id="penilaian">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">checklist</i>
                     </div>

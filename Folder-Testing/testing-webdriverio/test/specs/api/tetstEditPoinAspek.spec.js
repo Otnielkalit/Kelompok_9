2f58bcb3a32d596d52ca473a06ed362915c8d388
viranty <viranty21@gmail.com>
2023-12-11 10:52:21 +0700

    test api edit poin aspek in guru page

---
+++ Folder-Testing/testing-webdriverio/test/specs/api/tetstEditPoinAspek.spec.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const { expect } = require('chai');
+
+describe('Testing API Update Poin Aspek', () => {
+  it('should update a aspect point successfully', async () => {
+    try {
+      // ID kelas yang akan diupdate
+      const classIdToUpdate = '39';
+
+      // Data yang akan digunakan untuk update kelas
+      const updatedData = {
+        nama_poin: 'Tadikah Mesrah'
+      };
+
+      const updateUrl = `http://localhost:8081/kelas/${classIdToUpdate}`;
+      const response = await axios.put(updateUrl, updatedData);
+
+      // Periksa status respons
+      expect(response.status).to.equal(200);
+    } catch (error) {
+      throw new Error(error.response ? error.response.data : error.message);
+    }
+  });
+});
